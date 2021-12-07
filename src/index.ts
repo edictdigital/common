f1d1babf6fe0b7ff951ab260d22d50c8020adf32
Muhlis Murat Akay <akaymu@gmail.com>
2021-12-07 13:44:04 +0300

    [LP-27][MURAT] adding serialize decorator and serialize interceptor

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@
 import { BaseController } from "./base-crud/base.controller";
 import { BaseService, IBaseService } from "./base-crud/base.service";
 import { MainEntity } from "./base-crud/main.entity";
+import { SerializeInterceptor } from "./interceptors/serialize.interceptor";
+import { Serialize } from "./decorators/serialize.decorator";
 
 export {
   BaseController,
   BaseService,
   IBaseService,
   MainEntity,
+  SerializeInterceptor,
+  Serialize,
 };
