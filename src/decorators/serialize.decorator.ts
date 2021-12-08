8d33c24d8af9b1b9a6d9165f9154787df4c00cff
Muhlis Murat Akay <akaymu@gmail.com>
2021-12-08 03:41:03 +0300

    [LP-27][MURAT] removing to share Serialize Interceptor, Serialize decorator is enough to use it

--- src/decorators/serialize.decorator.ts
+++ src/decorators/serialize.decorator.ts
@@ -1,5 +1,12 @@
-import { UseInterceptors } from "@nestjs/common";
-import { SerializeInterceptor } from "../interceptors/serialize.interceptor";
+import { 
+  CallHandler, 
+  ExecutionContext, 
+  NestInterceptor, 
+  UseInterceptors 
+} from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 interface ClassConstructor {
   new(...args: any[]): {}
@@ -7,4 +14,24 @@ interface ClassConstructor {
 
 export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) { }
+
+  intercept(_context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled by the request handler. 
+    // console.log(context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out.
+        // console.log(data);
+
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      })
+    );
+  }
 }
